271244dfb49e7e415c4d8e3e57c71c8aca2ad006
Telphatech <telphatech@gmail.com>
2024-02-05 22:03:17 +0530

    files added

---
+++ lib/modules/home_admin/presentation/views/home_admin_mobile_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// TODO: PRANJAL
+
+class HomeAdminMobileView extends StatelessWidget {
+  const HomeAdminMobileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
