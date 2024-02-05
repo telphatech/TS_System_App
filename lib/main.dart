b7537fb2d633f52549f3083bf9bcae06fbaef7c5
Telphatech <telphatech@gmail.com>
2024-02-05 21:57:51 +0530

    Major changes in splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,17 @@ import 'package:flutter/material.dart';
 import './modules/splash/presentation/views/splash_screen_mobile_view.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home:
-        SplashScreenMobileView(), // Running SplashScreenMobileView as the home widget
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreenMobileView(),
+    );
+  }
 }
--- lib/modules/splash/presentation/views/splash_screen_mobile_view.dart
+++ lib/modules/splash/presentation/views/splash_screen_mobile_view.dart
@@ -1,27 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:ts_system/modules/login/sign_in/presentation/views/sign_in_mobile_view.dart';
 
-// TODO: Nikita - Work on Sign In Screen
-
 class SplashScreenMobileView extends StatelessWidget {
   const SplashScreenMobileView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Using Future.delayed to delay the navigation
-    Future.delayed(const Duration(seconds: 3), () {
-      // After 3 seconds, navigate to the next screen
+    Future.delayed(const Duration(seconds: 5), () {
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(
-            builder: (context) =>
-                SignInMobileView()), // Replace SignInScreen with  actual screen
+        MaterialPageRoute(builder: (context) => const SignInMobileView()),
       );
     });
 
     return Scaffold(
       backgroundColor: Colors.pink[800],
-      body: Center(
+      body: const Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
