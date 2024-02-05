ebe5570f5575638a9acdf1d655c5f53e5fb6dde3
Telphatech <telphatech@gmail.com>
2024-02-05 07:44:45 +0530

    Create Flutter Application

---
+++ android/app/src/main/kotlin/com/example/ts_system/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ts_system
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/modules/splash/presentation/views/splash_screen_mobile_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SplashScreenMobileView extends StatelessWidget {
+  const SplashScreenMobileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
